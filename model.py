9ddaa407404ec646d8013846161589679ea9e04a
Priya Patel <patelpriya19961996@gmail.com>
2023-07-04 17:26:02 -0500

    Added model.py

--- model.py
+++ model.py
@@ -10,3 +10,15 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+
+# Bulding CNN model
+class ConvolutionalNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 3, 1)
+        self.conv2 = nn.Conv2d(6, 16, 3, 1)
+        self.fc1 = nn.Linear(5*5*16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84,10)
+
+    
