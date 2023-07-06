d119c134391444c146492aa320925abe23337748
Priya Patel <patelpriya19961996@gmail.com>
2023-07-06 10:22:08 -0500

    Added model_training.py

---
+++ model_training.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torchvision.utils import make_grid
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
