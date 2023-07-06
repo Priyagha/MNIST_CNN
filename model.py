099ff4854c8702c0683443f4553b1ad9279b1217
Priya Patel <patelpriya19961996@gmail.com>
2023-07-06 09:48:27 -0500

    Creating cnn model

--- model.py
+++ model.py
@@ -21,4 +21,13 @@ class ConvolutionalNetwork(nn.Module):
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84,10)
 
-    
+    def forward(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = X.view(-1, 5*5*16)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = self.fc3(X)
+        return F.log_softmax(X, dim=1)
